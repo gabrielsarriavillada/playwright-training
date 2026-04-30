07dd60c893fd2eaf8a8f1ef33d750464679cb927
gabrielsarriavillada <gabrielsarriavillada@gmail.com>
2026-04-30 17:18:26 +0200

    Move conduitApi to fixture

--- fixtures/test.fixture.ts
+++ fixtures/test.fixture.ts
@@ -2,14 +2,16 @@ import { test as base, expect } from "@playwright/test";
 import { InputFieldsPage } from "../pages/InputFieldsPage";
 import { FormsPage } from "../pages/FormsPage";
 import { ConduitLoginPage } from "../pages/conduit/ConduitLoginPage";
+import { ConduitApi } from "../api/ConduitApi";
 
-type Pages = {
+type Fixtures = {
   inputFieldsPage: InputFieldsPage;
   formsPage: FormsPage;
   conduitLoginPage: ConduitLoginPage;
+  conduitApi: ConduitApi;
 };
 
-export const test = base.extend<Pages>({
+export const test = base.extend<Fixtures>({
   inputFieldsPage: async ({ page }, use) => {
     await use(new InputFieldsPage(page));
   },
@@ -18,6 +20,11 @@ export const test = base.extend<Pages>({
   },
   conduitLoginPage: async ({ page }, use) => {
     await use(new ConduitLoginPage(page));
+  },
+  conduitApi: async ({ request }, use) => {
+    const apiBaseUrl =
+      process.env.CONDUIT_API_BASE_URL ?? "https://api.realworld.show/api";
+    await use(new ConduitApi(request, apiBaseUrl));
   },
 });
 
--- tests/conduit/integration/conduit-login.spec.ts
+++ tests/conduit/integration/conduit-login.spec.ts
@@ -1,22 +1,14 @@
 import { test, expect } from "../../../fixtures/test.fixture";
-import { ConduitApi } from "../../../api/ConduitApi";
 
 test.describe("Conduit login flow", () => {
   test("should login through UI an API-created user", async ({
-    request,
+    conduitApi,
     conduitLoginPage,
     page,
   }) => {
-    const conduitApi = new ConduitApi(
-      request,
-      process.env.CONDUIT_API_BASE_URL || "https://api.realworld.show/api",
-    );
-
     const { user } = await conduitApi.createUserAndGetToken();
 
     await conduitLoginPage.open();
-
-    expect(page.url()).toBe("https://demo.realworld.show/login");
 
     await conduitLoginPage.login(user.email, user.password);
 
